dbf1ded1bc751babf54f7063ffd2d96665b6426b
johirjoy <joy.sd69@gmail.com>
2023-05-18 20:47:54 +0600

    bannder section complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
+import Aos from 'aos';
 import React from 'react';
 import { BiRightArrowAlt } from 'react-icons/bi';
 
 const Home = () => {
+    Aos.init();
     return (
         <>
             <div className="md:flex justify-around items-center gap-10 bg-[#ECE3E1] p-10">
-                <div className="mx-auto items-center space-y-3">
+                <div className="mx-auto items-center space-y-3" data-aos="fade-right">
                     <button className='text-center font-semibold text-white text-2xl px-7 py-4 bg-orange-400'>New Arrival</button>
                     <h2 className="text-7xl font-extrabold ">One Box Toy</h2>
                     <p>Flat 10% Off On Order Above $29.99</p>
@@ -13,7 +15,7 @@ const Home = () => {
                         <span className='pr-3'>Shop Now</span> <BiRightArrowAlt />
                     </button>
                 </div>
-                <div className='flex justify-center items-center'>
+                <div className='flex justify-center items-center' data-aos="zoom-in-left">
                     <img className='w-1/2' src="https://i.ibb.co/BC4zLKj/toy-banner.png" alt="Album" />
                 </div>
             </div>
